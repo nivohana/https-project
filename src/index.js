4ed71c55cc1a6ad9091ce646584082776e56ac0f
Niv Ohana <nivohana01@gmail.com>
2020-11-07 13:45:48 +0200

    commit-9.176-final

--- src/index.js
+++ src/index.js
@@ -3,6 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
+
+//global URL
+axios.defaults.baseURL='https://jsonplaceholder.typicode.com';
+
+//global Headers
+axios.defaults.headers.common['Authoraizations'] = 'AUTH TOKEN';
+axios.defaults.headers.post['content-Type'] = 'application/json';
+
+axios.interceptors.request.use(request => {
+    console.log(request);
+    return request;
+}, error =>{
+    console.log(error);
+    return Promise.reject(error)
+});
+
+
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();

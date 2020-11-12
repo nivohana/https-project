27d0debac4ab90bfac6a1a8227a9614a9fa86a24
Niv Ohana <nivohana01@gmail.com>
2020-11-12 15:58:57 +0200

    commit5

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
-import './Blog.css';
+import styles from './Blog.module.css';
 
 class Blog extends Component {
 
@@ -53,8 +53,17 @@ class Blog extends Component {
             ))
         }
         return (
-            <div>
-                <section className="Posts">
+            <div className={styles.Blog}>
+                <header>
+                    <nav>
+                        <ul>
+                            <li> <a href='/'> Home </a></li>
+                            <li> <a href='/new-post'> New-Post </a></li>
+                        </ul>
+                    </nav>
+
+                </header>
+                <section >
                     {posts}
 
                 </section>
